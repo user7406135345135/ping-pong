3b12efa917c922232582d1970ebca9ecc50fbe4d
user7406135345135 <mi.plus.bahir@gmail.com>
2024-11-05 16:46:54 +0300

    v.2.0

--- main.py
+++ main.py
@@ -35,8 +35,21 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 
-racket_1 = Player(0, 100, 10, 'racket.png', 50, 250)
-racket_2 = Player(750, 100, 10, 'racket.png', 50, 250)
+racket_1 = Player(0, 100, 10, 'racket.png', 50, 150)
+racket_2 = Player(750, 100, 10, 'racket.png', 50, 150)
+ball = GameSprite(350, 250, 100, 'ball.png', 60, 60)
+
+pygame.font.init()
+
+font1 = pygame.font.Font(None, 60)
+lose1 = font1.render('PLAYER 1 LOSE', True, (180, 0, 0))
+
+font2 = pygame.font.Font(None, 60)
+lose2 = font2.render('PLAYER 2 LOSE', True, (180, 0, 0))
+
+
+speed_x = 3
+speed_y = 3
 
 clock = pygame.time.Clock()
 game = True
@@ -49,7 +62,20 @@ while game:
     racket_2.reset()
     racket_1.update_left()
     racket_2.update_right()
+    ball.reset()
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    if ball.rect.y > win_height-50 or ball.rect.y <0:
+        speed_y *= -1
+    if pygame.sprite.collide_rect(racket_1, ball) or pygame.sprite.collide_rect(racket_2, ball):
+        speed_x *= -1
+    if ball.rect.x < -60:
+        finish = True
+        window.blit(lose1, (200, 200))
 
-    
+    if ball.rect.x > 800:
+        finish = True
+        window.blit(lose2, (200, 200))
+
     pygame.display.update()
     clock.tick(40)
